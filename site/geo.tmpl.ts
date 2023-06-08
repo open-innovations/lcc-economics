0cfbe3e11794aa1fd14cd25d961a284969ee43e5
Giles Dring <giles@dringtech.com>
2023-06-08 11:05:43 +0100

    Removing debug statement

--- site/geo.tmpl.ts
+++ site/geo.tmpl.ts
@@ -2,7 +2,7 @@ export const layout = 'templates/geo.njk';
 
 export default function* ({ geo }) {
   for (const place of geo) {
-    console.log(place)
+    // console.log(place) to check we're getting them OK
     yield {
       title: place,
       url: `/place/${ place.toLowerCase() }/`,
