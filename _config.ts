7dbeba984d753be4d69790d0bd192f264d6d2c5b
Giles Dring <giles@dringtech.com>
2023-06-20 14:43:10 +0100

    Adding OI Lume libraries

--- _config.ts
+++ _config.ts
@@ -3,10 +3,24 @@ import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 import metas from "lume/plugins/metas.ts"
 
+import oiCharts from "oi-lume-charts/mod.ts";
+
+import autoDependency from "https://deno.land/x/oi_lume_utils@v0.3.0/processors/auto-dependency.ts";
+import csvLoader from "https://deno.land/x/oi_lume_utils@v0.3.0/loaders/csv-loader.ts";
+
 const site = lume({
   src: "./site",
   location: new URL("https://open-innovations.github.io/lcc-economics/"),
 });
+
+site.loadData([".csv"], csvLoader);
+site.process([".html"], autoDependency);
+
+site.use(oiCharts({
+  font: {
+    // family: "chaparral-pro,sans-serif",
+  },
+}));
 
 site.use(metas());
 
