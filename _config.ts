8171b0a75d32cfd9403a025e146ccf04a2f22de0
Giles Dring <giles@dringtech.com>
2023-06-20 14:28:49 +0100

    Setting up site name and template

--- _config.ts
+++ _config.ts
@@ -1,10 +1,14 @@
 import lume from "lume/mod.ts";
+
 import basePath from "lume/plugins/base_path.ts";
+import metas from "lume/plugins/metas.ts"
 
 const site = lume({
   src: "./site",
   location: new URL("https://open-innovations.github.io/lcc-economics/"),
 });
+
+site.use(metas());
 
 site.use(basePath());
 
